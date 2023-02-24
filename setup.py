74aeaec1b69d2ce1287f9154a08dc1dcbd5bd637
Tobi Deusser <tobias.deusser@iais.fraunhofer.de>
2023-02-24 10:15:55 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="misusing-llms",
+    version="0.1",
+    author="Tobias Deusser",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "fluidml",
+        "datasets",
+        "tokenizers",
+        "pytorch-lightning",
+        "torch",
+        "tqdm",
+        "transformers",
+        "wandb",
+    ]
+)
