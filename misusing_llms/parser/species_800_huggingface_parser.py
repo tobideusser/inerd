32bf8e98db888119fb4ba9dfeccf5d2e58831479
Tobi Deusser <tobias.deusser@iais.fraunhofer.de>
2023-06-16 11:37:48 +0200

    added species-800 hf parser, but not working as google drive link where the dataset is stored is down currently (FIX THIS LATER!)

---
+++ misusing_llms/parser/species_800_huggingface_parser.py
@@ -0,0 +1,74 @@
+from typing import Optional
+
+from datasets import load_dataset, DatasetDict
+from tqdm import tqdm
+
+from misusing_llms.data_classes import Sentence, NERCorpus
+from misusing_llms.parser import BaseParser
+
+
+class Species800HuggingFaceParser(BaseParser):
+    def __init__(
+        self,
+        entity_separator_token: str,
+        type_content_separator_token: str,
+        type_mapping: bool = False,
+        debug_size: Optional[int] = None,
+        dataset_name: Optional[str] = None,
+    ):
+        if type_mapping:
+            ValueError("No type_mapping implemented for NCBI-disease.")
+        super().__init__(
+            type_mapping=None,
+            debug_size=debug_size,
+            entity_separator_token=entity_separator_token,
+            type_content_separator_token=type_content_separator_token,
+        )
+        self.dataset_name = dataset_name if dataset_name else "NCBI-disease"
+
+    def _parse_species800_split(self, dataset: DatasetDict, split_type: str):
+        parsed = []
+        i = 0
+        for sentence in tqdm(
+            dataset[split_type],
+            desc=f"Parsing {split_type}",
+            total=len(dataset[split_type]),
+        ):
+            entities = self._entity_tags_to_entity_dict(entity_tags=sentence["ner_tags"], words=sentence["tokens"])
+            parsed.append(
+                Sentence(
+                    id_=sentence["id"],
+                    words=sentence["tokens"],
+                    entity_tags=sentence["ner_tags"],
+                    entity_label=[self.entity_tag_to_label[entity_tag] for entity_tag in sentence["ner_tags"]],
+                    entities_anno=entities,
+                    entity_separator_token=self.entity_separator_token,
+                    type_content_separator_token=self.type_content_separator_token,
+                )
+            )
+            i += 1
+            if self.debug_size and i >= self.debug_size:
+                return parsed
+        return parsed
+
+    def parse(self) -> NERCorpus:
+        dataset = load_dataset("species_800")
+        corpus = {"train": [], "validation": [], "test": []}
+        entity_labels = dataset["train"].features["ner_tags"].feature.names
+        self.entity_tag_to_label = {i: entity_label for i, entity_label in enumerate(entity_labels)}
+        if self.type_mapping is not None:
+            for k, v in self.entity_tag_to_label.items():
+                for kk, vv in self.type_mapping.items():
+                    if kk in v:
+                        self.entity_tag_to_label[k] = self.entity_tag_to_label[k].replace(kk, vv)
+        for split_type in ["train", "validation", "test"]:
+            corpus[split_type] = self._parse_species800_split(dataset=dataset, split_type=split_type)
+
+        corpus_parsed = NERCorpus(
+            train=corpus["train"],
+            validation=corpus["validation"],
+            test=corpus["test"],
+            name=self.dataset_name,
+            entity_tag_to_label=self.entity_tag_to_label,
+        )
+        return corpus_parsed
